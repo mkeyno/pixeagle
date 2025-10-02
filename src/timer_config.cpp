7c81538e5e12278d396ced61ae84e46482a7d1ea
Mehrdad K <hrsk1980@gmail.com>
2025-10-03 12:37:34 +1300

    Update timer_config.cpp

--- src/timer_config.cpp
+++ src/timer_config.cpp
@@ -40,49 +40,35 @@
 
 #include <px4_arch/io_timer_hw_description.h>
 
-/* Timer allocation
- * TIM1_CH2: AUX GPIO 6 (PE11)
- * TIM1_CH3: PWM OUT 8 (PA10)
- * TIM1_CH4: WS2812B LEDs (PE14)
+/* Timer allocation - 8 PWM outputs only
+ *
  * TIM3_CH1: PWM OUT 5 (PC6)
- * TIM3_CH3: AUX GPIO 5 (PB0)
- * TIM3_CH4: PPM Input (PB1)  // UPDATED
  * TIM4_CH1: PWM OUT 1 (PD12)
  * TIM4_CH2: PWM OUT 2 (PD13)
  * TIM4_CH3: PWM OUT 3 (PD14)
  * TIM4_CH4: PWM OUT 4 (PD15)
- * TIM5_CH1: AUX GPIO 1 (PA0)
- * TIM5_CH2: AUX GPIO 2 (PA1)
- * TIM5_CH3: AUX GPIO 3 (PA2)
- * TIM5_CH4: AUX GPIO 4 (PA3)
  * TIM8_CH2: PWM OUT 6 (PC7)
- * TIM8_CH3: PWM OUT 7 (PC8)  // UPDATED
+ * TIM8_CH3: PWM OUT 7 (PC8)
+ * TIM1_CH3: PWM OUT 8 (PA10)
  */
 
-constexpr io_timers_t 		io_timers[MAX_IO_TIMERS] = {
-    initIOTimer(Timer::Timer1, DMA{DMA::Index1}), // TIM1 for PWM OUT 8, AUX GPIO 6, LEDs
-    initIOTimer(Timer::Timer3, DMA{DMA::Index1}), // TIM3 for PWM OUT 5, AUX GPIO 5, PPM
-    initIOTimer(Timer::Timer4, DMA{DMA::Index1}), // TIM4 for PWM OUT 1–4
-    initIOTimer(Timer::Timer5),                   // TIM5 for AUX GPIO 1–4
-    initIOTimer(Timer::Timer8, DMA{DMA::Index1}), // TIM8 for PWM OUT 6–7  // UPDATED: Added CH3 support
+constexpr io_timers_t io_timers[MAX_IO_TIMERS] = {
+    initIOTimer(Timer::Timer1, DMA{DMA::Index1}), // TIM1
+    initIOTimer(Timer::Timer3, DMA{DMA::Index1}), // TIM3
+    initIOTimer(Timer::Timer4, DMA{DMA::Index1}), // TIM4
+    initIOTimer(Timer::Timer8, DMA{DMA::Index1}), // TIM8
 };
 
-constexpr timer_io_channels_t 			timer_io_channels[MAX_TIMER_IO_CHANNELS] = {
-    initIOTimerChannel(io_timers, {Timer::Timer1, Timer::Channel3}, {GPIO::PortA, GPIO::Pin10}), // PWM OUT 8
-    initIOTimerChannel(io_timers, {Timer::Timer1, Timer::Channel2}, {GPIO::PortE, GPIO::Pin11}), // AUX GPIO 6
-    initIOTimerChannel(io_timers, {Timer::Timer3, Timer::Channel1}, {GPIO::PortC, GPIO::Pin6}),  // PWM OUT 5
-    initIOTimerChannel(io_timers, {Timer::Timer3, Timer::Channel3}, {GPIO::PortB, GPIO::Pin0}),  // AUX GPIO 5
-    initIOTimerChannel(io_timers, {Timer::Timer3, Timer::Channel4}, {GPIO::PortB, GPIO::Pin1}),  // PPM Input (UPDATED: Added as input channel)
+constexpr timer_io_channels_t timer_io_channels[MAX_TIMER_IO_CHANNELS] = {
     initIOTimerChannel(io_timers, {Timer::Timer4, Timer::Channel1}, {GPIO::PortD, GPIO::Pin12}), // PWM OUT 1
     initIOTimerChannel(io_timers, {Timer::Timer4, Timer::Channel2}, {GPIO::PortD, GPIO::Pin13}), // PWM OUT 2
     initIOTimerChannel(io_timers, {Timer::Timer4, Timer::Channel3}, {GPIO::PortD, GPIO::Pin14}), // PWM OUT 3
     initIOTimerChannel(io_timers, {Timer::Timer4, Timer::Channel4}, {GPIO::PortD, GPIO::Pin15}), // PWM OUT 4
-    initIOTimerChannel(io_timers, {Timer::Timer5, Timer::Channel1}, {GPIO::PortA, GPIO::Pin0}),  // AUX GPIO 1
-    initIOTimerChannel(io_timers, {Timer::Timer5, Timer::Channel2}, {GPIO::PortA, GPIO::Pin1}),  // AUX GPIO 2
-    initIOTimerChannel(io_timers, {Timer::Timer5, Timer::Channel3}, {GPIO::PortA, GPIO::Pin2}),  // AUX GPIO 3
-    initIOTimerChannel(io_timers, {Timer::Timer5, Timer::Channel4}, {GPIO::PortA, GPIO::Pin3}),  // AUX GPIO 4
+    initIOTimerChannel(io_timers, {Timer::Timer3, Timer::Channel1}, {GPIO::PortC, GPIO::Pin6}),  // PWM OUT 5
     initIOTimerChannel(io_timers, {Timer::Timer8, Timer::Channel2}, {GPIO::PortC, GPIO::Pin7}),  // PWM OUT 6
-    initIOTimerChannel(io_timers, {Timer::Timer8, Timer::Channel3}, {GPIO::PortC, GPIO::Pin8}),  // PWM OUT 7 (UPDATED: Timer/channel)
+    initIOTimerChannel(io_timers, {Timer::Timer8, Timer::Channel3}, {GPIO::PortC, GPIO::Pin8}),  // PWM OUT 7
+    initIOTimerChannel(io_timers, {Timer::Timer1, Timer::Channel3}, {GPIO::PortA, GPIO::Pin10}), // PWM OUT 8
 };
 
-constexpr io_timers_channel_mapping_t 		io_timers_channel_mapping = initIOTimerChannelMapping(io_timers, timer_io_channels);
+constexpr io_timers_channel_mapping_t io_timers_channel_mapping =
+    initIOTimerChannelMapping(io_timers, timer_io_channels);
