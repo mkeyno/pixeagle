a50206157ff84bc0f52408705dcd4eec83125d28
Mehrdad K <hrsk1980@gmail.com>
2025-12-06 21:55:47 +1300

    Revise board configuration for Pixeagle
    Updated hardware configuration for Pixeagle board, including GPIO definitions, PWM settings, and ADC channels.

--- src/board_config.h
+++ src/board_config.h
@@ -1,153 +1,240 @@
-/**
- * @file board_config.h
- *
- * Pixeagle internal definitions - CORRECTED VERSION
- *
- * Hardware configuration for Pixeagle board (STM32H743VIT6 MCU, version V1C00)
- */
-
 #pragma once
+
+/****************************************************************************************************
+ * Included Files
+ ****************************************************************************************************/
 
 #include <px4_platform_common/px4_config.h>
 #include <nuttx/compiler.h>
 #include <stdint.h>
+
 #include <stm32_gpio.h>
 
-// Change this from 5 to 4 (only using TIM1, TIM3, TIM4, TIM8)
-#define BOARD_NUM_IO_TIMERS 4
 
-// Keep this as 8 for your 8 PWM outputs
-#define BOARD_NUM_TIMER_IO_CHANNELS 8
+extern   uint32_t board_hw_version;
+extern   uint32_t board_hw_revision;
 
-// These should match
-#define DIRECT_PWM_OUTPUT_CHANNELS  8
-#define DIRECT_INPUT_TIMER_CHANNELS 8
+/* Hardware version definition */
+#define HW_VER_REV(v,r) ((((uint32_t)(v)) << 16) | ((uint32_t)(r)))
+#define VER00 HW_VER_REV(0x0,0x0)
 
- 
-/************************************************************************************
- * Board Control Definitions
- ************************************************************************************/
-#define BOARDIOC_INIT      0x0000
-#define BOARDIOC_RESET     0x0001  /* Reset to bootloader if arg=1, normal reset if arg=0 */
-#define BOARDIOC_POWEROFF  0x0002
+
+
 /****************************************************************************************************
  * Definitions
  ****************************************************************************************************/
 
-/* No PX4IO (FMU-only design) */
-#define BOARD_USES_PX4IO_VERSION       0
+/* PX4IO connection configuration */
 
-/* Single hardware version for Pixeagle */
-#define BOARD_NUM_SPI_CFG_HW_VERSIONS 1
-#define V1C00   HW_VER_REV(0x1,0x0)
+#define BOARD_USES_PX4IO_VERSION       2
+#define PX4IO_SERIAL_DEVICE            "/dev/ttyS5"
+#define PX4IO_SERIAL_TX_GPIO           GPIO_UART5_TX
+#define PX4IO_SERIAL_RX_GPIO           GPIO_UART5_RX
+#define PX4IO_SERIAL_BASE              STM32_UART5_BASE
+#define PX4IO_SERIAL_VECTOR            STM32_IRQ_UART5
+#define PX4IO_SERIAL_TX_DMAMAP         DMAMAP_UART5_TX
+#define PX4IO_SERIAL_RX_DMAMAP         DMAMAP_UART5_RX
+#define PX4IO_SERIAL_RCC_REG           STM32_RCC_APB1LENR
+#define PX4IO_SERIAL_RCC_EN            RCC_APB1LENR_UART5EN
+#define PX4IO_SERIAL_CLOCK             STM32_PCLK1_FREQUENCY
+#define PX4IO_SERIAL_BITRATE           1500000               /* 1.5Mbps -> max rate for IO */
 
-/* Standard PX4 SPI selector macro */
-#define PX4_MK_SPI_SEL(bus, dev) (((bus) << 8) | (dev))
+/* LEDs are driven with push open drain to support Anode to 5V or 3.3V */
+/* Note: Using WS2812B on PE14 for RGB LED (bitbang implementation) */
+/* PE13 and PE15 available for debugging/status */
 
-/* ADC - Reference from board.h (no redefs) */
+#define GPIO_nLED_RED        /* PE13 */  (GPIO_OUTPUT|GPIO_OPENDRAIN|GPIO_SPEED_50MHz|GPIO_OUTPUT_SET|GPIO_PORTE|GPIO_PIN13)
+#define GPIO_nLED_GREEN      /* PE15 */  (GPIO_OUTPUT|GPIO_OPENDRAIN|GPIO_SPEED_50MHz|GPIO_OUTPUT_SET|GPIO_PORTE|GPIO_PIN15)
+
+#define BOARD_HAS_CONTROL_STATUS_LEDS      1
+#define BOARD_OVERLOAD_LED     LED_RED
+#define BOARD_ARMED_STATE_LED  LED_GREEN
+
+/* WS2812B RGB LED - using bitbang on PE14 */
+#define GPIO_LED_WS2812B     /* PE14 */  (GPIO_OUTPUT|GPIO_PUSHPULL|GPIO_SPEED_100MHz|GPIO_OUTPUT_CLEAR|GPIO_PORTE|GPIO_PIN14)
+
+/* ADC channels */
+
+/* Define GPIO pins used as ADC N.B. Channel numbers must match below */
+
 #define PX4_ADC_GPIO  \
-    /* PC0 */  GPIO_ADC1_IN10,  /* Analog sensor 1 */ \
-    /* PC1 */  GPIO_ADC1_IN11,  /* Analog sensor 2 */ \
-    /* PC2 */  GPIO_ADC1_IN12,  /* Battery voltage */ \
-    /* PC3 */  GPIO_ADC1_IN13   /* Battery current */
+	/* PC0 */  GPIO_ADC123_INP10, \
+	/* PC1 */  GPIO_ADC123_INP11, \
+	/* PC2 */  GPIO_ADC123_INP12, \
+	/* PC3 */  GPIO_ADC12_INP13
 
-/* SPI Bus definitions */
-#define PX4_SPI_BUS_SENSORS      1
-#define PX4_SPI_BUS_RAMTRON      2  
-#define PX4_SPI_BUS_EXT          3
-#define PX4_SPI_BUS_ICM42688P    4
+/* Define Channel numbers must match above GPIO pin IN(n)*/
 
-/* SPI Device definitions */
-#define PX4_SPIDEV_BMI088_ACC          PX4_MK_SPI_SEL(PX4_SPI_BUS_SENSORS,0)
-#define PX4_SPIDEV_BMI088_GYR          PX4_MK_SPI_SEL(PX4_SPI_BUS_SENSORS,1)
-#define PX4_SPIDEV_FRAM                PX4_MK_SPI_SEL(PX4_SPI_BUS_RAMTRON,0)
-#define PX4_SPIDEV_MICROSD             PX4_MK_SPI_SEL(PX4_SPI_BUS_RAMTRON,1)
-#define PX4_SPIDEV_EXT0                PX4_MK_SPI_SEL(PX4_SPI_BUS_EXT,0)
-#define PX4_SPIDEV_ICM42688P           PX4_MK_SPI_SEL(PX4_SPI_BUS_ICM42688P,0)
+#define ADC_SCALED_V5_CHANNEL               /* PC0 */  10
+#define ADC_SCALED_VDD_3V3_SENSORS_CHANNEL  /* PC1 */  11
+#define ADC_BATTERY_VOLTAGE_CHANNEL         /* PC2 */  12
+#define ADC_BATTERY_CURRENT_CHANNEL         /* PC3 */  13
 
-#define SPIDEV_FRAM 0
+#define ADC_CHANNELS \
+	((1 << ADC_SCALED_V5_CHANNEL)              | \
+	 (1 << ADC_SCALED_VDD_3V3_SENSORS_CHANNEL) | \
+	 (1 << ADC_BATTERY_VOLTAGE_CHANNEL)        | \
+	 (1 << ADC_BATTERY_CURRENT_CHANNEL))
 
-#define BOARD_NUMBER_OF_PWM_DRIVERS 8  // Standard FMU PWM groups for metadata
-/* SPI clock frequencies */
-#define PX4_SPI_BUS_FREQ_SENSORS      20000000
-#define PX4_SPI_BUS_FREQ_RAMTRON      20000000
-#define PX4_SPI_BUS_FREQ_EXT          10000000
-#define PX4_SPI_BUS_FREQ_ICM42688P    20000000
+/* HW has to large of R termination on ADC todo:change when HW value is chosen */
 
-/* Sensor DRDY interrupts - THESE ARE WHAT CONTROL INTERRUPT-DRIVEN DATA */
-#define GPIO_BMI088_ACC_INT1    (GPIO_INPUT|GPIO_PULLUP|GPIO_EXTI|GPIO_PORTC|GPIO_PIN4)
-#define GPIO_BMI088_GYR_INT1    (GPIO_INPUT|GPIO_PULLUP|GPIO_EXTI|GPIO_PORTC|GPIO_PIN5)
-#define GPIO_ICM42688P_DRDY     (GPIO_INPUT|GPIO_PULLUP|GPIO_EXTI|GPIO_PORTE|GPIO_PIN3)
+#define BOARD_ADC_OPEN_CIRCUIT_V     (5.6f)
 
-/* Number of SPI bus hardware versions */
-#define BOARD_NUM_SPI_CFG_HW_VERSIONS 1
+/* Safety switch and alarm */
+
+#define GPIO_SAFETY_SWITCH_IN   /* PE12 */ (GPIO_INPUT|GPIO_PULLDOWN|GPIO_PORTE|GPIO_PIN12)
+#define GPIO_SAFETY_SWITCH_LED  /* PE15 */ (GPIO_OUTPUT|GPIO_PUSHPULL|GPIO_SPEED_2MHz|GPIO_OUTPUT_CLEAR|GPIO_PORTE|GPIO_PIN15)
+#define GPIO_ALARM_OUTPUT       /* PE13 */ (GPIO_OUTPUT|GPIO_PUSHPULL|GPIO_SPEED_2MHz|GPIO_OUTPUT_CLEAR|GPIO_PORTE|GPIO_PIN13)
 
 
 
-/* I2C Bus definitions - PX4 abstraction layer */
-#define PX4_I2C_BUS_EXT                1
-#define PX4_I2C_BUS_IST8310_BMP388     3
-#define PX4_I2C_BUS_BMP390             4
+/* PWM - 8 channels total */
 
-/* I2C Device addresses - PX4 abstraction layer */
-#define PX4_I2C_OBDEV_IST8310          0x0e
-#define PX4_I2C_OBDEV_BMP388           0x77
-#define PX4_I2C_OBDEV_BMP390           0x77
+#define DIRECT_PWM_OUTPUT_CHANNELS  8
+#define BOARD_NUM_IO_TIMERS 4
 
-/* GPIO List - Consolidated PX4 init list (uses board.h names directly) */
+/* Power supply control and monitoring GPIOs */
+/* PA15 - Reset peripheral power rails (sensors via PA15, active high) */
+
+#define GPIO_VDD_3V3_SENSORS_EN     /* PA15 */ (GPIO_OUTPUT|GPIO_PUSHPULL|GPIO_SPEED_2MHz|GPIO_OUTPUT_CLEAR|GPIO_PORTA|GPIO_PIN15)
+
+/* Define True logic Power Control in arch agnostic form */
+
+#define VDD_3V3_SENSORS_EN(on_true)    px4_arch_gpiowrite(GPIO_VDD_3V3_SENSORS_EN, (on_true))
+
+/* Tone alarm output - PE13 can be used for alarm */
+
+#define TONE_ALARM_TIMER        1   /* timer 1 */
+#define TONE_ALARM_CHANNEL      3   /* PE14 TIM1_CH3   */
+
+#define GPIO_BUZZER_1           /* PE13 */ (GPIO_OUTPUT|GPIO_PUSHPULL|GPIO_SPEED_2MHz|GPIO_OUTPUT_CLEAR|GPIO_PORTE|GPIO_PIN13)
+
+#define GPIO_TONE_ALARM_IDLE    GPIO_BUZZER_1
+#define GPIO_TONE_ALARM         GPIO_BUZZER_1
+
+/* USB OTG FS
+ *
+ * PA9  OTG_FS_VBUS VBUS sensing
+ */
+#define GPIO_OTGFS_VBUS         /* PA9 */ (GPIO_INPUT|GPIO_PULLDOWN|GPIO_SPEED_100MHz|GPIO_PORTA|GPIO_PIN9)
+
+/* High-resolution timer */
+#define HRT_TIMER               2  /* use timer1 for the HRT */
+#define HRT_TIMER_CHANNEL       1  /* use capture/compare channel 3 PA10 */
+
+/* PWM input driver - using PB1 (PPM input) attached to timer3 */
+
+#define PWMIN_TIMER                       3
+#define PWMIN_TIMER_CHANNEL    /* T3C4 */ 4
+#define GPIO_PWM_IN            /* PB1 */  GPIO_TIM3_CH4IN
+
+/* RC Input - SBUS on USART3 */
+#define RC_SERIAL_PORT                    "/dev/ttyS2"
+
+/* SDIO */
+#define SDIO_SLOTNO                    0  /* Only one slot */
+#define SDIO_MINOR                     0
+
+/* SD card bringup does not work if performed on the IDLE thread because it
+ * will cause waiting.  Use either:
+ *
+ *  CONFIG_BOARDCTL=y, OR
+ *  CONFIG_BOARD_INITIALIZE=y && CONFIG_BOARD_INITTHREAD=y
+ */
+
+#if defined(CONFIG_BOARD_INITIALIZE) && !defined(CONFIG_BOARDCTL) && \
+   !defined(CONFIG_BOARD_INITTHREAD)
+#  warning SDIO initialization cannot be perfomed on the IDLE thread
+#endif
+
+/* By Providing BOARD_ADC_USB_CONNECTED (using the px4_arch abstraction)
+ * this board support the ADC system_power interface, and therefore
+ * provides the true logic GPIO BOARD_ADC_xxxx macros.
+ */
+#define BOARD_ADC_USB_CONNECTED (px4_arch_gpioread(GPIO_OTGFS_VBUS))
+#define BOARD_ADC_USB_VALID     (px4_arch_gpioread(GPIO_OTGFS_VBUS))
+
+/* Board never powers off the Servo rail */
+
+#define BOARD_ADC_SERVO_VALID     (1)
+
+#define BOARD_ADC_BRICK_VALID     (1)
+
+/* Raspberry Pi CM4 status pin */
+#define GPIO_RPI_CM4_STATUS     /* PD11 */ (GPIO_INPUT|GPIO_PULLDOWN|GPIO_PORTD|GPIO_PIN11)
+
+/* This board provides a DMA pool and APIs */
+#define BOARD_DMA_ALLOC_POOL_SIZE 5120
+
+/* This board provides the board_on_reset interface */
+
+#define BOARD_HAS_ON_RESET 1
+#define SDMMC_PIN_OFF(def) (((def) & (GPIO_PORT_MASK | GPIO_PIN_MASK)) | (GPIO_INPUT|GPIO_FLOAT|GPIO_SPEED_2MHz))
+
 #define PX4_GPIO_INIT_LIST { \
-    PX4_ADC_GPIO, \
-    GPIO_VDD_5V_PERIPH_EN, \
-    GPIO_WS2812B_LED, \
-    GPIO_SAFETY_LED, \
-    GPIO_SAFETY_SWITCH_IN, \
-    GPIO_BUZZER, \
-    GPIO_CAN1_TX, \
-    GPIO_CAN1_RX, \
-    GPIO_CAN2_TX, \
-    GPIO_CAN2_RX, \
-    GPIO_PPM_IN, \
-    GPIO_CM4_STATUS, \
-    GPIO_BMI088_ACC_INT1, \
-    GPIO_BMI088_GYR_INT1, \
-    GPIO_ICM42688P_DRDY \
-}
+		PX4_ADC_GPIO,                     \
+		GPIO_CAN1_TX,                     \
+		GPIO_CAN1_RX,                     \
+		GPIO_CAN2_TX,                     \
+		GPIO_CAN2_RX,                     \
+		GPIO_VDD_3V3_SENSORS_EN,          \
+		GPIO_SAFETY_SWITCH_IN,            \
+		GPIO_SAFETY_SWITCH_LED,           \
+		GPIO_ALARM_OUTPUT,                \
+		GPIO_LED_WS2812B,                 \
+		GPIO_RPI_CM4_STATUS,              \
+		SDMMC_PIN_OFF(GPIO_SDMMC1_D0),    \
+		SDMMC_PIN_OFF(GPIO_SDMMC1_D1),    \
+		SDMMC_PIN_OFF(GPIO_SDMMC1_D2),    \
+		SDMMC_PIN_OFF(GPIO_SDMMC1_D3),    \
+		SDMMC_PIN_OFF(GPIO_SDMMC1_CMD),   \
+		GPIO_TONE_ALARM_IDLE,             \
+	}
 
-/* Board identification - Use board.h values directly */
-#define BOARD_TYPE                     100      /* Must match hw_config.h and .prototype */
-#define BOARD_FLASH_SECTORS            16
-#define BOARD_FLASH_SIZE               (2 * 1024 * 1024)
-
-/* Console and features */
 #define BOARD_ENABLE_CONSOLE_BUFFER
-#define BOARD_HAS_ON_RESET             1
-
-/* HW Rev and Ver detection */
-#define BOARD_HAS_HW_SPLIT_VERSIONING
-
-/* Dummy HW Revision Sensing - use PC13, PC14 */
-#define GPIO_HW_REV_DRIVE     (GPIO_OUTPUT|GPIO_PUSHPULL|GPIO_SPEED_2MHz|GPIO_OUTPUT_CLEAR|GPIO_PORTC|GPIO_PIN13)
-#define GPIO_HW_REV_SENSE     (GPIO_INPUT|GPIO_FLOAT|GPIO_PORTC|GPIO_PIN14)
-#define ADC_HW_REV_SENSE_CHANNEL    ADC1_CH(10)  /* Use existing PC0 */
-
-/* Dummy HW Version Sensing - use the same pins (it's just dummy detection) */
-#define GPIO_HW_VER_DRIVE     GPIO_HW_REV_DRIVE   /* Same as revision drive */
-#define GPIO_HW_VER_SENSE     GPIO_HW_REV_SENSE   /* Same as revision sense */
-#define ADC_HW_VER_SENSE_CHANNEL    ADC_HW_REV_SENSE_CHANNEL  /* Same ADC channel */
-
-
-#define HW_INFO_INIT_PREFIX           "PX4_"
-
-#define HW_INFO_INIT \
-        {HW_INFO_INIT_PREFIX"FMUV6C", "PIXEAGLE", "V1.0"},
 
 __BEGIN_DECLS
 
-/* Function declarations */
+/****************************************************************************************************
+ * Public Types
+ ****************************************************************************************************/
+
+/****************************************************************************************************
+ * Public data
+ ****************************************************************************************************/
+
+#ifndef __ASSEMBLY__
+
+/****************************************************************************************************
+ * Public Functions
+ ****************************************************************************************************/
+
+/****************************************************************************
+ * Name: stm32_sdio_initialize
+ *
+ * Description:
+ *   Initialize SDIO-based MMC/SD card support
+ *
+ ****************************************************************************/
+
+int stm32_sdio_initialize(void);
+
+/****************************************************************************************************
+ * Name: stm32_spiinitialize
+ *
+ * Description:
+ *   Called to configure SPI chip select GPIO pins for the board.
+ *
+ ****************************************************************************************************/
+
 extern void stm32_spiinitialize(void);
+
 extern void stm32_usbinitialize(void);
+
 extern void board_peripheral_reset(int ms);
 
 #include <px4_platform_common/board_common.h>
+
+#endif /* __ASSEMBLY__ */
 
 __END_DECLS
