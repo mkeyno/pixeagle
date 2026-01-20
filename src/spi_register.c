4dc0ea2fb803fcd14e9a49f2c6d97c2e36dee1c1
Mehrdad K <hrsk1980@gmail.com>
2026-01-20 14:53:48 +1300

    Update spi_register.c

--- src/spi_register.c
+++ src/spi_register.c
@@ -1,38 +1,34 @@
 /****************************************************************************
  * boards/px4/pixeagle/src/spi_register.c
  *
- * SPI device registration callbacks
+ * Board-specific SPI callback registration for Pixeagle.
  *
+ * NOTE: stm32_spiXselect and stm32_spiXstatus are NOT defined here because
+ * they are provided by the common PX4/NuttX SPI driver (libarch_spi), which
+ * uses the configuration from spi.cpp to toggle pins automatically.
+ * * We only provide stm32_spiXregister because CONFIG_SPI_CALLBACK is enabled.
  ****************************************************************************/
-
 #include <nuttx/config.h>
 #include <nuttx/spi/spi.h>
+#include <stm32_gpio.h>
+#include <stm32_spi.h>
+#include "board_config.h"
 
 /****************************************************************************
- * Name: stm32_spi1/2/3/4register
- *
- * Description:
- *   Called to configure SPI chip select GPIO pins. These are dummy
- *   implementations since Pixeagle doesn't use media change detection.
- *
+ * Name: stm32_spi1register
  ****************************************************************************/
+int stm32_spi1register(FAR struct spi_dev_s *dev, spi_mediachange_t callback, FAR void *arg){    return OK;}
+/****************************************************************************
+ * Name: stm32_spi2register
+ ****************************************************************************/
+int stm32_spi2register(FAR struct spi_dev_s *dev, spi_mediachange_t callback, FAR void *arg){    return OK;}
 
-void stm32_spi1register(struct spi_dev_s *dev, spi_mediachange_t callback, void *arg)
-{
-	/* No media change callback for SPI1 */
-}
+/****************************************************************************
+ * Name: stm32_spi3register
+ ****************************************************************************/
+int stm32_spi3register(FAR struct spi_dev_s *dev, spi_mediachange_t callback, FAR void *arg){    return OK;}
+/****************************************************************************
+ * Name: stm32_spi4register
+ ****************************************************************************/
+int stm32_spi4register(FAR struct spi_dev_s *dev, spi_mediachange_t callback, FAR void *arg){    return OK;}
 
-void stm32_spi2register(struct spi_dev_s *dev, spi_mediachange_t callback, void *arg)
-{
-	/* No media change callback for SPI2 */
-}
-
-void stm32_spi3register(struct spi_dev_s *dev, spi_mediachange_t callback, void *arg)
-{
-	/* No media change callback for SPI3 */
-}
-
-void stm32_spi4register(struct spi_dev_s *dev, spi_mediachange_t callback, void *arg)
-{
-	/* No media change callback for SPI4 */
-}
